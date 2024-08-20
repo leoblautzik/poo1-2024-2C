bc48037de27d27ec4098b3df0d12ba922f3b0af4
leoblautzik <leoblautzik@gmail.com>
2024-08-19 22:43:32 -0300

    Ejercicio06 resuelto en java

---
+++ java/ejercicios_loo/EjerciciosLOO/src/ejercicios/App.java
@@ -0,0 +1,7 @@
+package ejercicios;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ java/ejercicios_loo/EjerciciosLOO/src/ejercicios/Ejercicio06.java
@@ -0,0 +1,43 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ * Leer tres valores numéricos enteros, indicar cual es el mayor,
+ * cuál es el del medio y cuál el menor.
+ * Considerar que los tres valores son diferentes.
+ */
+
+public class Ejercicio06 {
+
+    public static void main(String[] args) {
+        int mayor, medio, menor;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un valor entero: ");
+        int a = sc.nextInt();
+        System.out.println("Ingrese otro valor entero: ");
+        int b = sc.nextInt();
+        System.out.println("Ingrese un valor entero mas: ");
+        int c = sc.nextInt();
+
+        mayor = a;
+        if (b > a) {
+            medio = a;
+            mayor = b;
+        } else {
+            medio = b;
+        }
+        if (c > mayor) {
+            menor = medio;
+            medio = mayor;
+            mayor = c;
+        } else if (c > medio) {
+            menor = medio;
+            medio = c;
+        } else {
+            menor = c;
+        }
+
+        System.out.println("Mayor: " + mayor + ", Medio: " + medio + ", Menor: " + menor);
+    }
+}
