bc8a5abdeb0477652a0dc35e9d2f16512bc07708
Leonardo Blautzik <leoblautzik@gmail.com>
2024-08-25 20:12:55 -0300

    ejercicio_26_27 de loo sin warnings

--- python/ejercicios_loo/ejercicio_26_27_str.py
+++ python/ejercicios_loo/ejercicio_26_27_str.py
@@ -67,18 +67,19 @@ def sin_acentos(a):
     return b
 
 
-s = "Hola"
-print(s)
-print(invertir(s))
+print("Hola Mundo")
+print(invertir("Hola Mundo"))
 
-p = "neuquen"
-print(es_palindromo(p))
+print(es_palindromo("Neuquén"))
 print(es_palindromo("a"))
 print(es_palindromo(""))
 print(es_palindromo("      A mi loca  Colima    "))
 print(es_palindromo("A Mercedes, ese de crema."))
 print(es_palindromo("A la catalana banal, atácala."))
-cadena = "Adivina ya te opina, ya ni miles origina, ya ni cetro me domina, ya ni monarcas, a repaso ni mulato carreta, acaso nicotina, ya ni cita vecino, anima cocina, pedazo gallina, cedazo terso nos retoza de canilla goza, de pánico camina, ónice vaticina, ya ni tocino saca, a terracota luminosa pera, sacra nómina y ánimo de mortecina, ya ni giros elimina, ya ni poeta, ya ni vida"
-print(es_palindromo(cadena))
+print(
+    es_palindromo(
+        "Adivina ya te opina, ya ni miles origina, ya ni cetro me domina, ya ni monarcas, a repaso ni mulato carreta, acaso nicotina, ya ni cita vecino, anima cocina, pedazo gallina, cedazo terso nos retoza de canilla goza, de pánico camina, ónice vaticina, ya ni tocino saca, a terracota luminosa pera, sacra nómina y ánimo de mortecina, ya ni giros elimina, ya ni poeta, ya ni vida"
+    )
+)
 print(es_palindromo("aña"))
 print(es_palindromo("ñañañ"))
