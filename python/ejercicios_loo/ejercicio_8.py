c8deccd674a08c2c72ba41e2beee15cb79ec6999
Leonardo Blautzik <leoblautzik@gmail.com>
2024-08-25 20:01:31 -0300

    Ejemplos de encapsulamiento con Test Unitarios en python (Punto y Circulo)

--- python/ejercicios_loo/ejercicio_8.py
+++ python/ejercicios_loo/ejercicio_8.py
@@ -3,8 +3,8 @@ enteros positivos, se pide informar, por cada uno,
 si el valor ingresado es par o impar. 
 Para indicar el final se ingresará un valor cero o negativo.
 """
-v = int(input("Ingrese un valor entero positivo: "))
 
+v = int(input("Ingrese un valor entero positivo: "))
 while v > 0:
     if v % 2 == 0:
         print("Es par")
